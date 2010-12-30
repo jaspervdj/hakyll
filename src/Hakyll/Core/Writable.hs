86fafe6e61f71428f933f62a3c68cb3819c189c9
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-12-30 13:49:58 +0100

    Add CopyFile newtype

--- src/Hakyll/Core/Writable.hs
+++ src/Hakyll/Core/Writable.hs
@@ -1,9 +1,16 @@
 -- | Describes writable items; items that can be saved to the disk
 --
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving,
+        DeriveDataTypeable #-}
 module Hakyll.Core.Writable
     ( Writable (..)
+    , CopyFile (..)
     ) where
+
+import System.Directory (copyFile)
+
+import Data.Binary (Binary)
+import Data.Typeable (Typeable)
 
 -- | Describes an item that can be saved to the disk
 --
@@ -13,3 +20,11 @@ class Writable a where
 
 instance Writable [Char] where
     write = writeFile
+
+-- | Newtype construct around 'FilePath' which will copy the file directly
+--
+newtype CopyFile = CopyFile {unCopyFile :: FilePath}
+                 deriving (Show, Eq, Ord, Binary, Typeable)
+
+instance Writable CopyFile where
+    write dst (CopyFile src) = copyFile src dst
