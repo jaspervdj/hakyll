9e88440102842ca7fbed342e7f29ab9ea1dfea6f
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-01-07 14:22:15 +0100

    Restructure hakyllWith for metacompilers

--- src/Hakyll/Core/Run.hs
+++ src/Hakyll/Core/Run.hs
@@ -39,91 +39,28 @@ hakyll :: Rules -> IO ()
 hakyll rules = do
     store <- makeStore "_store"
     provider <- fileResourceProvider
-    evalStateT
-        (runReaderT
-            (unHakyll (hakyllWith rules provider store)) undefined) undefined
-
-data HakyllState = HakyllState
-    { hakyllCompilers :: [(Identifier, Compiler () CompileRule)]
-    }
+    let ruleSet = runRules rules provider
+        compilers = rulesCompilers ruleSet
+    runReaderT (unHakyll (addNewCompilers [] compilers)) $
+        env ruleSet provider store
+  where
+    env ruleSet provider store = HakyllEnvironment
+        { hakyllRoute            = rulesRoute ruleSet
+        , hakyllResourceProvider = provider
+        , hakyllStore            = store
+        , hakyllModified         = S.empty
+        }
 
 data HakyllEnvironment = HakyllEnvironment
     { hakyllRoute            :: Route
     , hakyllResourceProvider :: ResourceProvider
     , hakyllStore            :: Store
+    , hakyllModified         :: Set Identifier
     }
 
 newtype Hakyll a = Hakyll
-    { unHakyll :: ReaderT HakyllEnvironment (StateT HakyllState IO) a
+    { unHakyll :: ReaderT HakyllEnvironment IO a
     } deriving (Functor, Applicative, Monad)
-
-hakyllWith :: Rules -> ResourceProvider -> Store -> Hakyll ()
-hakyllWith rules provider store = Hakyll $ do
-    let -- Get the rule set
-        ruleSet = runRules rules provider
-
-        -- Get all identifiers and compilers
-        compilers = rulesCompilers ruleSet
-
-        -- Get all dependencies
-        dependencies = flip map compilers $ \(id', compiler) ->
-            let deps = runCompilerDependencies compiler provider
-            in (id', deps)
-
-        -- Create a compiler map
-        compilerMap = M.fromList compilers
-
-        -- Create the graph
-        graph = fromList dependencies
-
-    liftIO $ do
-        putStrLn "Writing dependency graph to dependencies.dot..."
-        writeDot "dependencies.dot" show graph
-
-    -- Check which items are up-to-date
-    modified' <- liftIO $ modified provider store $ map fst compilers
-
-    let -- Try to reduce the graph
-        reducedGraph = filterObsolete modified' graph
-
-    liftIO $ do
-        putStrLn "Writing reduced graph to reduced.dot..."
-        writeDot "reduced.dot" show reducedGraph
-
-    let -- Solve the graph
-        ordered = solveDependencies reducedGraph
-
-        -- Join the order with the compilers again
-        orderedCompilers = map (id &&& (compilerMap M.!)) ordered
-
-        -- Fetch the routes
-        route' = rulesRoute ruleSet
-
-    -- Generate all the targets in order
-    _ <- mapM (addTarget route' modified') orderedCompilers
-
-    liftIO $ putStrLn "DONE."
-  where
-    addTarget route' modified' (id', comp) = do
-        let url = runRoute route' id'
-        
-        -- Check if the resource was modified
-        let isModified = id' `S.member` modified'
-
-        -- Run the compiler
-        ItemRule compiled <- liftIO $
-            runCompiler comp id' provider url store isModified
-        liftIO $ putStrLn $ "Generated target: " ++ show id'
-
-        case url of
-            Nothing -> return ()
-            Just r  -> liftIO $ do
-                putStrLn $ "Routing " ++ show id' ++ " to " ++ r
-                let path = "_site" </> r
-                makeDirectories path
-                write path compiled
-
-        liftIO $ putStrLn ""
 
 -- | Return a set of modified identifiers
 --
@@ -134,3 +71,87 @@ modified :: ResourceProvider     -- ^ Resource provider
 modified provider store ids = fmap S.fromList $ flip filterM ids $ \id' ->
     if resourceExists provider id' then resourceModified provider id' store
                                    else return False
+
+-- | Add a number of compilers and continue using these compilers
+--
+addNewCompilers :: [(Identifier, Compiler () CompileRule)]
+                -- ^ Remaining compilers yet to be run
+                -> [(Identifier, Compiler () CompileRule)]
+                -- ^ Compilers to add
+                -> Hakyll ()
+addNewCompilers oldCompilers newCompilers = Hakyll $ do
+    -- Get some information
+    provider <- hakyllResourceProvider <$> ask
+    store <- hakyllStore <$> ask
+
+    let -- All compilers
+        compilers = oldCompilers ++ newCompilers
+
+        -- Get all dependencies for the compilers
+        dependencies = flip map compilers $ \(id', compiler) ->
+            let deps = runCompilerDependencies compiler provider
+            in (id', deps)
+
+        -- Create a compiler map (Id -> Compiler)
+        compilerMap = M.fromList compilers
+
+        -- Create the dependency graph
+        graph = fromList dependencies
+
+    -- Check which items are up-to-date. This only needs to happen for the new
+    -- compilers
+    modified' <- liftIO $ modified provider store $ map fst compilers
+
+    let -- Try to reduce the graph using this modified information
+        reducedGraph = filterObsolete modified' graph
+
+    let -- Solve the graph
+        ordered = solveDependencies reducedGraph
+
+        -- Join the order with the compilers again
+        orderedCompilers = map (id &&& (compilerMap M.!)) ordered
+
+    -- Now run the ordered list of compilers
+    local (updateModified modified') $ unHakyll $ runCompilers orderedCompilers
+  where
+    -- Add the modified information for the new compilers
+    updateModified modified' env = env
+        { hakyllModified = hakyllModified env `S.union` modified'
+        }
+
+runCompilers :: [(Identifier, Compiler () CompileRule)]
+             -- ^ Ordered list of compilers
+             -> Hakyll ()
+             -- ^ No result
+runCompilers [] = return ()
+runCompilers ((id', compiler) : compilers) = Hakyll $ do
+    -- Obtain information
+    route' <- hakyllRoute <$> ask
+    provider <- hakyllResourceProvider <$> ask
+    store <- hakyllStore <$> ask
+    modified' <- hakyllModified <$> ask
+
+    let -- Determine the URL
+        url = runRoute route' id'
+    
+        -- Check if the resource was modified
+        isModified = id' `S.member` modified'
+
+    -- Run the compiler
+    result <- liftIO $ runCompiler compiler id' provider url store isModified
+    liftIO $ putStrLn $ "Generated target: " ++ show id'
+
+    let ItemRule compiled = result
+
+    case url of
+        Nothing -> return ()
+        Just r  -> liftIO $ do
+            putStrLn $ "Routing " ++ show id' ++ " to " ++ r
+            let path = "_site" </> r
+            makeDirectories path
+            write path compiled
+
+    liftIO $ putStrLn ""
+
+    -- Continue for the remaining compilers
+    unHakyll $ runCompilers compilers 
