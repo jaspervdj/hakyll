d89728ff2d962f74573088ab4f95151bfe6413a8
Paolo Veronelli <paolo.veronelli@gmail.com>
2011-03-04 17:40:57 +0100

    wider inotify sensibilty, create, delete , movein , moveout deleting a file is not working

--- src-inotify/Hakyll/Web/Preview/Poll.hs
+++ src-inotify/Hakyll/Web/Preview/Poll.hs
@@ -5,16 +5,16 @@ module Hakyll.Web.Preview.Poll
     ) where
 
 import Control.Monad (forM_, when)
-import Data.Set (Set)
-import qualified Data.Set as S
 import System.FilePath (takeDirectory, (</>))
-import Data.List (isPrefixOf)
+import Data.List (isPrefixOf, nub)
+import Data.Set (Set,toList)
 
 import System.INotify
 
 import Hakyll.Core.Configuration
 import Hakyll.Core.ResourceProvider
 import Hakyll.Core.Identifier
+import Debug.Trace
 
 -- | Calls the given callback when the directory tree changes
 --
@@ -26,26 +26,14 @@ previewPoll _ resources callback = do
     -- Initialize inotify
     inotify <- initINotify
 
-    let -- A set of file paths
-        paths = S.map (toFilePath . unResource) resources
-
-        -- A list of directories. Run it through a set so we have every
-        -- directory only once.
-        directories = S.toList $ S.map (notEmpty . takeDirectory) paths
-
-        -- Problem: we can't add a watcher for "". So we make sure a directory
+    let -- Problem: we can't add a watcher for "". So we make sure a directory
         -- name is not empty
         notEmpty "" = "."
         notEmpty x  = x
 
-        -- Execute the callback when path is known
-        ifResource path =
-            let path' = if "./" `isPrefixOf` path then drop 2 path else path
-            in when (path' `S.member` paths) callback
+        -- A list of directories. Run it through a set so we have every
+        -- directory only once.
+        directories = nub . map (notEmpty . takeDirectory . toFilePath . unResource) $ toList resources
 
     -- Add a watcher for every directory
-    forM_ directories $ \directory -> do
-        _ <- addWatch inotify [Modify] directory $ \e -> case e of
-            (Modified _ (Just p)) -> ifResource $ directory </> p
-            _                     -> return ()
-        return ()
+    forM_ directories $ \directory -> addWatch inotify [MoveIn,MoveOut,Delete,Create] directory $ const callback
