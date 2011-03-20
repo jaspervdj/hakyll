99233f830cead0dea265eb5ec708f781295e734e
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-03-20 15:14:42 +0100

    Binary instance for DirectedGraph

--- src/Hakyll/Core/DirectedGraph/Internal.hs
+++ src/Hakyll/Core/DirectedGraph/Internal.hs
@@ -1,17 +1,21 @@
 -- | Internal structure of the DirectedGraph type. Not exported outside of the
 -- library.
 --
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Hakyll.Core.DirectedGraph.Internal
     ( Node (..)
     , DirectedGraph (..)
     ) where
 
 import Prelude hiding (reverse, filter)
+import Control.Applicative ((<$>), (<*>))
 import Data.Monoid (Monoid, mempty, mappend)
 import Data.Set (Set)
 import Data.Map (Map)
 import qualified Data.Map as M
 import qualified Data.Set as S
+
+import Data.Binary (Binary, put, get)
 
 -- | A node in the directed graph
 --
@@ -19,6 +23,10 @@ data Node a = Node
     { nodeTag        :: a      -- ^ Tag identifying the node
     , nodeNeighbours :: Set a  -- ^ Edges starting at this node
     } deriving (Show)
+
+instance (Binary a, Ord a) => Binary (Node a) where
+    put (Node t n) = put t >> put n
+    get = Node <$> get <*> get
 
 -- | Append two nodes. Useful for joining graphs.
 --
@@ -33,7 +41,7 @@ appendNodes (Node t1 n1) (Node t2 n2)
 -- | Type used to represent a directed graph
 --
 newtype DirectedGraph a = DirectedGraph {unDirectedGraph :: Map a (Node a)}
-                        deriving (Show)
+                        deriving (Show, Binary)
 
 -- | Allow users to concatenate different graphs
 --
