d004dc19996c5d1a96a36c46c2580c3f0db33261
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-05-25 11:24:33 +0200

    Test for UnixFilter

--- src/Hakyll/Core/Resource/Provider/Dummy.hs
+++ src/Hakyll/Core/Resource/Provider/Dummy.hs
@@ -7,15 +7,17 @@ module Hakyll.Core.Resource.Provider.Dummy
 import Data.Map (Map)
 import qualified Data.Map as M
 
-import qualified Data.ByteString.Lazy.Char8 as LBC
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
+import Data.ByteString.Lazy (ByteString)
 
 import Hakyll.Core.Resource
 import Hakyll.Core.Resource.Provider
 
 -- | Create a dummy 'ResourceProvider'
 --
-dummyResourceProvider :: Map String String -> IO ResourceProvider
+dummyResourceProvider :: Map String ByteString -> IO ResourceProvider
 dummyResourceProvider vfs = makeResourceProvider
     (map Resource (M.keys vfs))
+    (return . TL.unpack . TL.decodeUtf8 . (vfs M.!) . unResource)
     (return . (vfs M.!) . unResource)
-    (return . LBC.pack . (vfs M.!) . unResource)
