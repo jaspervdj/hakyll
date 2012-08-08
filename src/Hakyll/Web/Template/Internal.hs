7b7c2da3c6bacda82fd25ebe091d78256cf904fc
Andrew Miller <ak.miller@auckland.ac.nz>
2012-08-08 15:24:59 +1200

    Make Template support strict and lazy ByteStrings as well as String.

--- src/Hakyll/Web/Template/Internal.hs
+++ src/Hakyll/Web/Template/Internal.hs
@@ -1,38 +1,85 @@
 -- | Module containing the template data structure
 --
-{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveDataTypeable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveDataTypeable, FlexibleInstances  #-}
 module Hakyll.Web.Template.Internal
     ( Template (..)
     , TemplateElement (..)
+    , TemplateString (..)
     ) where
 
 import Control.Applicative ((<$>))
 
 import Data.Binary (Binary, get, getWord8, put, putWord8)
 import Data.Typeable (Typeable)
-
+import Data.Monoid
+import qualified Data.ByteString.Char8 as SBS
+import qualified Data.ByteString.Lazy.Char8 as LBS
 import Hakyll.Core.Writable
+import Hakyll.Core.Compiler
+import Hakyll.Core.Resource
+import Control.Arrow
+import Data.String
+import Data.List
+
+-- | The typeclass of string-like types that can be used to build a template.
+class (Monoid a, IsString a, Binary a, Show a, Eq a, Typeable a, Writable a, Ord a) => TemplateString a where
+  tsNull :: a -> Bool
+  tsIsPrefixOf :: a -> a -> Bool
+  tsToString :: a -> String
+  tsDrop :: Int -> a -> a
+  tsBreak :: (Char -> Bool) -> a -> (a, a)
+  tsBreak f = tsSpan (not . f)
+  tsSpan :: (Char -> Bool) -> a -> (a, a)
+  tsSpan f = tsBreak (not . f)
+  tsGetResource :: Compiler Resource a
+
+instance TemplateString [Char] where
+  tsNull = null
+  tsIsPrefixOf = isPrefixOf
+  tsToString = id
+  tsDrop = drop
+  tsBreak = break
+  tsSpan = span
+  tsGetResource = getResourceString
+
+instance TemplateString SBS.ByteString where
+  tsNull = SBS.null
+  tsIsPrefixOf = SBS.isPrefixOf
+  tsToString = SBS.unpack
+  tsDrop n = SBS.drop (fromIntegral n)
+  tsBreak = SBS.break
+  tsSpan = SBS.span
+  tsGetResource = getResourceLBS >>^ (SBS.concat . LBS.toChunks)
+  
+instance TemplateString LBS.ByteString where
+  tsNull = LBS.null
+  tsIsPrefixOf = LBS.isPrefixOf
+  tsToString = LBS.unpack
+  tsDrop n = LBS.drop (fromIntegral n)
+  tsBreak = LBS.break
+  tsSpan = LBS.span
+  tsGetResource = getResourceLBS
 
 -- | Datatype used for template substitutions.
 --
-newtype Template = Template
-    { unTemplate :: [TemplateElement]
+newtype Template a = Template
+    { unTemplate :: [TemplateElement a]
     }
     deriving (Show, Eq, Binary, Typeable)
 
-instance Writable Template where
+instance TemplateString a => Writable (Template a) where
     -- Writing a template is impossible
     write _ _ = return ()
 
 -- | Elements of a template.
 --
-data TemplateElement
-    = Chunk String
+data TemplateElement a
+    = Chunk a
     | Key String
     | Escaped
     deriving (Show, Eq, Typeable)
 
-instance Binary TemplateElement where
+instance TemplateString a => Binary (TemplateElement a) where
     put (Chunk string)    = putWord8 0 >> put string
     put (Key key)  = putWord8 1 >> put key
     put (Escaped) = putWord8 2
--- src/Hakyll/Web/Template/Read/Hakyll.hs
+++ src/Hakyll/Web/Template/Read/Hakyll.hs
@@ -1,5 +1,6 @@
 -- | Read templates in Hakyll's native format
 --
+{-# LANGUAGE OverloadedStrings #-}
 module Hakyll.Web.Template.Read.Hakyll
     ( readTemplate
     ) where
@@ -11,25 +12,25 @@ import Hakyll.Web.Template.Internal
 
 -- | Construct a @Template@ from a string.
 --
-readTemplate :: String -> Template
+readTemplate :: TemplateString a => a -> Template a
 readTemplate = Template . readTemplate'
   where
-    readTemplate' [] = []
     readTemplate' string
-        | "$$" `isPrefixOf` string =
-            Escaped : readTemplate' (drop 2 string)
-        | "$" `isPrefixOf` string =
-            case readKey (drop 1 string) of
-                Just (key, rest) -> Key key : readTemplate' rest
-                Nothing          -> Chunk "$" : readTemplate' (drop 1 string)
+        | tsNull string = []
+        | "$$" `tsIsPrefixOf` string =
+            Escaped : readTemplate' (tsDrop 2 string)
+        | "$" `tsIsPrefixOf` string =
+            case readKey (tsDrop 1 string) of
+                Just (key, rest) -> Key (tsToString key) : readTemplate' rest
+                Nothing          -> Chunk "$" : readTemplate' (tsDrop 1 string)
         | otherwise =
-            let (chunk, rest) = break (== '$') string
+            let (chunk, rest) = tsBreak (== '$') string
             in Chunk chunk : readTemplate' rest
 
     -- Parse an key into (key, rest) if it's valid, and return
     -- Nothing otherwise
     readKey string =
-        let (key, rest) = span isAlphaNum string
-        in if not (null key) && "$" `isPrefixOf` rest
-            then Just (key, drop 1 rest)
+        let (key, rest) = tsSpan isAlphaNum string
+        in if not (tsNull key) && "$" `tsIsPrefixOf` rest
+            then Just (key, tsDrop 1 rest)
             else Nothing
