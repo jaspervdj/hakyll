571de045f570eb84159dba50d9f6e2a7b4584879
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-05-15 10:11:22 +0530

    now readTemplate is just fromString

--- src/Hakyll/Web/Template/Read.hs
+++ src/Hakyll/Web/Template/Read.hs
@@ -6,8 +6,7 @@ module Hakyll.Web.Template.Read
 
 
 --------------------------------------------------------------------------------
-import           Data.List                    (isPrefixOf)
-
+import           Data.String                  (fromString)
 
 --------------------------------------------------------------------------------
 import           Hakyll.Web.Template.Internal
@@ -16,26 +15,4 @@ import           Hakyll.Web.Template.Internal
 --------------------------------------------------------------------------------
 -- | Construct a @Template@ from a string.
 readTemplate :: String -> Template
-readTemplate = Template . readTemplate'
-  where
-    readTemplate' [] = []
-    readTemplate' string
-        | "$$" `isPrefixOf` string =
-            Escaped : readTemplate' (drop 2 string)
-        | "$" `isPrefixOf` string =
-            case readKey (drop 1 string) of
-                Just (key, rest) -> Key key : readTemplate' rest
-                Nothing          -> Chunk "$" : readTemplate' (drop 1 string)
-        | otherwise =
-            let (chunk, rest) = break (== '$') string
-            in Chunk chunk : readTemplate' rest
-
-    -- Parse an key into (key, rest) if it's valid, and return
-    -- Nothing otherwise
-    readKey string =
-        let (key, rest) = span validKeyChar string
-        in if not (null key) && "$" `isPrefixOf` rest
-            then Just (key, drop 1 rest)
-            else Nothing
-
-    validKeyChar x = x `notElem` ['$', '\n', '\r']
+readTemplate = fromString
