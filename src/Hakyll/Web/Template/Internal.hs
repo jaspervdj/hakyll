90591e45f4b7c1d1e747c345dfbd4389fe82bb8a
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-05-15 10:09:32 +0530

    added IsString instance for Template fixes #150.

--- src/Hakyll/Web/Template/Internal.hs
+++ src/Hakyll/Web/Template/Internal.hs
@@ -11,6 +11,8 @@ module Hakyll.Web.Template.Internal
 --------------------------------------------------------------------------------
 import           Control.Applicative  ((<$>))
 import           Data.Binary          (Binary, get, getWord8, put, putWord8)
+import           Data.List            (isPrefixOf)
+import           Data.String          (IsString(..))
 import           Data.Typeable        (Typeable)
 
 
@@ -40,7 +42,6 @@ data TemplateElement
     | Escaped
     deriving (Show, Eq, Typeable)
 
-
 --------------------------------------------------------------------------------
 instance Binary TemplateElement where
     put (Chunk string)    = putWord8 0 >> put string
@@ -53,3 +54,28 @@ instance Binary TemplateElement where
             2 -> return Escaped
             _ -> error $  "Hakyll.Web.Template.Internal: "
                        ++ "Error reading cached template"
+                       
+--------------------------------------------------------------------------------
+instance IsString Template where
+  fromString = Template . readTemplate'
+    where readTemplate' [] = []
+          readTemplate' string
+            | "$$" `isPrefixOf` string =
+              Escaped : readTemplate' (drop 2 string)
+            | "$" `isPrefixOf` string =
+                case readKey (drop 1 string) of
+                  Just (key, rest) -> Key key : readTemplate' rest
+                  Nothing          -> Chunk "$" : readTemplate' (drop 1 string)
+            | otherwise =
+                  let (chunk, rest) = break (== '$') string
+                  in Chunk chunk : readTemplate' rest
+
+          -- Parse an key into (key, rest) if it's valid, and return
+          -- Nothing otherwise
+          readKey string =
+            let (key, rest) = span validKeyChar string
+            in if not (null key) && "$" `isPrefixOf` rest
+               then Just (key, drop 1 rest)
+               else Nothing
+
+          validKeyChar x = x `notElem` ['$', '\n', '\r']
