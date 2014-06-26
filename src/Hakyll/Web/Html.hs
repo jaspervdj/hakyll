jaspervdj/hakyll

commit 3ee6f28c10797b0ccee63f6144c14f1430e24564

Fix #223: `demoteHeaders` should not modify anything in the document, apart from the headers

src/Hakyll/Web/Html.hs
@@ -80,6 +80,7 @@ renderTags' :: [TS.Tag String] -> String
 renderTags' = TS.renderTagsOptions TS.renderOptions
     { TS.optRawTag   = (`elem` ["script", "style"]) . map toLower
     , TS.optMinimize = (`S.member` minimize) . map toLower
+    , TS.optEscape   = id
     }
   where
     -- A list of elements which must be minimized

commit 261277dfc59ddeb3b228a2a857052c70baffbeaf

Fix build failure introduced by 3ee6f28
Fixes #270

src/Hakyll/Web/Html.hs
@@ -77,7 +77,7 @@ withUrls f = withTags tag
 -- | Customized TagSoup renderer. The default TagSoup renderer escape CSS
 -- within style tags, and doesn't properly minimize.
 renderTags' :: [TS.Tag String] -> String
-renderTags' = TS.renderTagsOptions TS.renderOptions
+renderTags' = TS.renderTagsOptions TS.RenderOptions
     { TS.optRawTag   = (`elem` ["script", "style"]) . map toLower
     , TS.optMinimize = (`S.member` minimize) . map toLower
     , TS.optEscape   = id