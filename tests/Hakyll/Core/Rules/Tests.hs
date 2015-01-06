b7940aca564a88e4e05b8a3d8e582aab7aed1ef9
Jasper Van der Jeugt <m@jaspervdj.be>
2015-01-06 13:57:13 +0100

    Add a test for #300

--- tests/Hakyll/Core/Rules/Tests.hs
+++ tests/Hakyll/Core/Rules/Tests.hs
@@ -8,7 +8,9 @@ module Hakyll.Core.Rules.Tests
 --------------------------------------------------------------------------------
 import           Data.IORef                     (IORef, newIORef, readIORef,
                                                  writeIORef)
+import qualified Data.Map                       as M
 import qualified Data.Set                       as S
+import           System.FilePath                ((</>))
 import           Test.Framework                 (Test, testGroup)
 import           Test.HUnit                     (Assertion, assert, (@=?))
 
@@ -44,22 +46,29 @@ case01 = do
             runRoutes routes provider i >>= \(r, _) -> Just ex @=? r
 
     -- Test that we have some identifiers and that the routes work out
-    assert $ all (`S.member` identifiers) expected
-    checkRoute "example.html" "example.md"
-    checkRoute "example.md"   (sv "raw" "example.md")
-    checkRoute "example.md"   (sv "nav" "example.md")
-    checkRoute "example.mv1"  (sv "mv1" "example.md")
-    checkRoute "example.mv2"  (sv "mv2" "example.md")
+    S.fromList expected @=? identifiers
+    checkRoute "example.html"    "example.md"
+    checkRoute "example.md"      (sv "raw" "example.md")
+    checkRoute "example.md"      (sv "nav" "example.md")
+    checkRoute "example.mv1"     (sv "mv1" "example.md")
+    checkRoute "example.mv2"     (sv "mv2" "example.md")
+    checkRoute "food/example.md" (sv "metadataMatch" "example.md")
     readIORef ioref >>= assert
     cleanTestEnv
   where
     sv g     = setVersion (Just g)
     expected =
-        [ "example.md"
-        , "russian.md"
-        , sv "raw" "example.md"
+        [                    "example.md"
+        , sv "raw"           "example.md"
+        , sv "metadataMatch" "example.md"
+        , sv "nav"           "example.md"
+        , sv "mv1"           "example.md"
+        , sv "mv2"           "example.md"
+
+        ,          "russian.md"
         , sv "raw" "russian.md"
-        , sv "nav" "example.md"
+        , sv "mv1" "russian.md"
+        , sv "mv2" "russian.md"
         ]
 
 
@@ -78,6 +87,11 @@ rules01 ioref = do
     match "*.md" $ version "raw" $ do
         route idRoute
         compile getResourceString
+
+    version "metadataMatch" $
+        matchMetadata "*.md" (\md -> M.lookup "subblog" md == Just "food") $ do
+            route $ customRoute $ \id' -> "food" </> toFilePath id'
+            compile getResourceString
 
     -- Regression test
     version "nav" $ match (fromList ["example.md"]) $ do
