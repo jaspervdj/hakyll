3aa07ea334f14df19019947c2eef37b34bae2625
Nikolaos S. Papaspyrou <nickie@softlab.ntua.gr>
2016-10-31 14:45:13 +0200

    Add comment

--- src/Data/Yaml/Extended.hs
+++ src/Data/Yaml/Extended.hs
@@ -13,6 +13,8 @@ toString :: Value -> Maybe String
 toString (String t)     = Just (T.unpack t)
 toString (Bool   True)  = Just "true"
 toString (Bool   False) = Just "false"
+-- | Make sure that numeric fields containing integer numbers are shown as
+-- | integers (i.e., "42" instead of "42.0").
 toString (Number d) | isInteger d = Just (formatScientific Fixed (Just 0) d)
                     | otherwise   = Just (show d)
 toString _              = Nothing
