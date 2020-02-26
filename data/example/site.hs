f953b54c5534f511e0a42cd9a5243759a7dc9c6a
Liang-Ting Chen <liang.ting.chen.tw@gmail.com>
2020-02-26 12:32:03 +0000

    Remove constant field title for index.html in the example site
    In the default site generated by Hakyll, `index.html` has specified the filed `title` in its front matter, but it will always been overridden by `constField "title" "Home"` in `site.hs`.
    
    It is a very minor issue, but it may cause confusion to newcomers.

--- data/example/site.hs
+++ data/example/site.hs
@@ -49,7 +49,6 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
                     defaultContext
 
             getResourceBody
