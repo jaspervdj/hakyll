77afcbc2937a4ee5db9666c1f3e0c090914d3980
Jasper Van der Jeugt <m@jaspervdj.be>
2020-12-06 19:24:06 +0100

    Pandoc 2.11 compatibility (#826)
    * Pandoc 2.11 compatibility
    
    * Bump stack.yaml
    
    * Bump stack dependencies

--- lib/Hakyll/Web/Pandoc/Binary.hs
+++ lib/Hakyll/Web/Pandoc/Binary.hs
@@ -4,9 +4,6 @@ module Hakyll.Web.Pandoc.Binary where
 
 import           Data.Binary        (Binary (..))
 
-import qualified Text.CSL           as CSL
-import qualified Text.CSL.Reference as REF
-import qualified Text.CSL.Style     as STY
 import           Text.Pandoc
 
 --------------------------------------------------------------------------------
@@ -18,7 +15,6 @@ instance Binary Caption
 instance Binary Cell
 instance Binary ColSpan
 instance Binary ColWidth
-instance Binary CSL.Reference
 instance Binary Citation
 instance Binary CitationMode
 instance Binary Format
@@ -27,17 +23,9 @@ instance Binary ListNumberDelim
 instance Binary ListNumberStyle
 instance Binary MathType
 instance Binary QuoteType
-instance Binary REF.CLabel
-instance Binary REF.CNum
-instance Binary REF.Literal
-instance Binary REF.RefDate
-instance Binary REF.RefType
-instance Binary REF.Season
 instance Binary Row
 instance Binary RowHeadColumns
 instance Binary RowSpan
-instance Binary STY.Agent
-instance Binary STY.Formatted
 instance Binary TableBody
 instance Binary TableFoot
 instance Binary TableHead
