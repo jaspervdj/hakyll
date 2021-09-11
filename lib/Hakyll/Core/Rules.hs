c1fcc2a8fea5b939d2fda04ae7c5b2272f674632
Fraser Tweedale <frase@frase.id.au>
2021-09-11 15:14:35 +1000

    remove forceCompile
    Commit 6885325146aa46adf255c55de0e0345a0f84961e introduced the
    `forceCompile` rules modifier.  But `recompilingUnsafeCompiler` is
    the preferable way to achieve this.  So remove `forceCompile`.
    
    This is a breaking change, but `forceCompile` was very new, so it is
    not expected to impact many users.

--- lib/Hakyll/Core/Rules.hs
+++ lib/Hakyll/Core/Rules.hs
@@ -29,7 +29,6 @@ module Hakyll.Core.Rules
     , preprocess
     , Dependency (..)
     , rulesExtraDependencies
-    , forceCompile
     ) where
 
 
@@ -222,11 +221,3 @@ rulesExtraDependencies deps rules =
             | (i, c) <- rulesCompilers ruleSet
             ]
         }
-
-
---------------------------------------------------------------------------------
--- | Force the item(s) to always be recompiled, whether or not the
--- dependencies are out of date.  This can be useful if you are using
--- I/O to generate part (or all) of an item.
-forceCompile :: Rules a -> Rules a
-forceCompile = rulesExtraDependencies [AlwaysOutOfDate]
