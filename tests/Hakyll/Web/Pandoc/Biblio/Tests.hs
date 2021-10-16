90ee51c889e4b839e53c2012c0c79c151a23132d
Benjamin Eskola <ben@eskola.uk>
2021-10-16 18:22:27 +0100

    Add tests for readPandocBiblio with CSL YAML bibliography

--- tests/Hakyll/Web/Pandoc/Biblio/Tests.hs
+++ tests/Hakyll/Web/Pandoc/Biblio/Tests.hs
@@ -25,6 +25,7 @@ import           TestSuite.Util
 tests :: TestTree
 tests = testGroup "Hakyll.Web.Pandoc.Biblio.Tests" $
     [ goldenTest01
+    , goldenTest02
     ]
 
 --------------------------------------------------------------------------------
@@ -36,7 +37,7 @@ goldenTest01 :: TestTree
 goldenTest01 =
     goldenVsString
         "biblio01"
-        (goldenTestsDataDir </> "biblio01.golden")
+        (goldenTestsDataDir </> "cites-meijer.golden")
         (do
             -- Code lifted from https://github.com/jaspervdj/hakyll-citeproc-example.
             logger <- Logger.new Logger.Error
@@ -52,6 +53,39 @@ goldenTest01 =
                 match "default.html" $ compile templateCompiler
                 match "chicago.csl" $ compile cslCompiler
                 match "refs.bib"    $ compile biblioCompiler
+                match "page.markdown" $ do
+                    route $ setExtension "html"
+                    compile $
+                        myPandocBiblioCompiler >>=
+                        loadAndApplyTemplate "default.html" defaultContext
+
+            output <- fmap LBS.fromStrict $ B.readFile $
+                    destinationDirectory testConfiguration </> "page.html"
+
+            cleanTestEnv
+
+            return output)
+
+goldenTest02 :: TestTree
+goldenTest02 =
+    goldenVsString
+        "biblio02"
+        (goldenTestsDataDir </> "cites-meijer.golden")
+        (do
+            -- Code lifted from https://github.com/jaspervdj/hakyll-citeproc-example.
+            logger <- Logger.new Logger.Error
+            let config = testConfiguration { providerDirectory = goldenTestsDataDir }
+            _ <- run RunModeNormal config logger $ do
+                let myPandocBiblioCompiler = do
+                        csl <- load "chicago.csl"
+                        bib <- load "refs.yaml"
+                        getResourceBody >>=
+                            readPandocBiblio defaultHakyllReaderOptions csl bib >>=
+                            return . writePandoc
+
+                match "default.html" $ compile templateCompiler
+                match "chicago.csl" $ compile cslCompiler
+                match "refs.yaml"    $ compile biblioCompiler
                 match "page.markdown" $ do
                     route $ setExtension "html"
                     compile $
