20efc952a55fe8e1f1900de829ce262288349dd0
Malte Neuss <malteneuss@users.noreply.github.com>
2021-11-28 20:51:57 +0100

    Add Identifier documentation

--- lib/Hakyll/Core/Identifier.hs
+++ lib/Hakyll/Core/Identifier.hs
@@ -1,12 +1,15 @@
 --------------------------------------------------------------------------------
--- | An identifier is a type used to uniquely identify an item. An identifier is
--- conceptually similar to a file path. Examples of identifiers are:
+-- | An identifier is a type used to uniquely identify an item. An identifier
+-- conceptually corresponds to a file path. Examples of identifiers are:
 --
 -- * @posts/foo.markdown@
 --
 -- * @index@
 --
 -- * @error/404@
+--
+-- See 'Identifier' for details.
+
 {-# LANGUAGE DeriveDataTypeable         #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Hakyll.Core.Identifier
@@ -32,6 +35,66 @@ import           GHC.Exts            (IsString, fromString)
 
 
 --------------------------------------------------------------------------------
+{- | A key data type to identify a compiled 'Hakyll.Core.Item.Item' in the 'Hakyll.Core.Store.Store'.
+Conceptually, it's a combination of a file path and a version name.
+The version is used only when a file is
+compiled within a rule using the 'version' wrapper function
+(the same source file
+can be compiled into several items in the store, so the version exists to distinguish
+them).
+Use functions like 'fromFilePath', 'setVersion', 'Hakyll.Core.Metadata.getMatches' to build an 'Identifier'.
+
+=== __Usage Examples__
+Normally, compiled items are saved to the store by 'Hakyll.Core.Rules.Rules' with an automatic, implicit identifier
+and loaded from the store by the user in another rule with a manual, explicit identifier.
+
+__Identifiers when using match__.
+Using 'Hakyll.Core.Rules.match' builds an implicit identifier that corresponds to the expanded, relative path
+of the source file on disk (relative to the project directory configured
+with 'Hakyll.Core.Configuration.providerDirectory'):
+
+@
+-- e.g. file on disk: 'posts\/hakyll.md'
+match "posts/*" $ do                                      -- saved with implicit identifier 'posts\/hakyll.md'
+    compile pandocCompiler
+
+match "about/*" $ do
+    compiledPost <- load (fromFilePath "posts/hakyll.md") -- load with explicit identifier
+    ...
+@
+
+__Identifiers when using create__.
+Using 'Hakyll.Core.Rules.create' (thereby inventing a file path with no underlying file on disk)
+builds an implicit identifier that corresponds to the invented file path:
+
+@
+create ["index.html"] $ do                            -- saved with implicit identifier 'index.html'
+    makeItem ("Hello world" :: String)
+
+match "about/*" $ do
+    compiledIndex <- load (fromFilePath "index.html") -- load with an explicit identifier
+    ...
+@
+
+__Identifiers when using versions__.
+With 'Hakyll.Core.Rules.version' the same file can compiled into several items on the store.
+A version name is needed to distinguish them:
+
+@
+-- e.g. file on disk: 'posts\/hakyll.md'
+match "posts/*" $ do                                  -- saved with implicit identifier ('posts\/hakyll.md', no-version)
+    compile $ pandocCompiler
+
+match "posts/*" $ version "raw" $ do                  -- saved with implicit identifier ('posts\/hakyll.md', version 'raw')
+    compile getResourceBody
+
+match "about/*" $ do
+    compiledPost <- load (fromFilePath "posts/hakyll.md")                      -- load no-version version
+    rawPost <- load . setVersion (Just "raw") $ fromFilePath "posts/hakyll.md" -- load version 'raw'
+    ...
+@
+Use 'setVersion' to specify or replace the version of an identifier like @fromFilePath "posts/hakyll.md"@.
+-}
 data Identifier = Identifier
     { identifierVersion :: Maybe String
     , identifierPath    :: String
@@ -44,7 +107,7 @@ instance Binary Identifier where
     get = Identifier <$> get <*> get
 
 
---------------------------------------------------------------------------------
+--------------------fromFilePath "posts/hakyll.md"------------------------------------------------------------
 instance IsString Identifier where
     fromString = fromFilePath
 
