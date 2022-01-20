e4404e943b9a524c2a1bfc834be71f712327754c
Alexander Batischev <eual.jp@gmail.com>
2022-01-20 21:14:44 +0300

    Fix whitespace errors in lib/Hakyll/Web/Pandoc.hs

--- lib/Hakyll/Web/Pandoc.hs
+++ lib/Hakyll/Web/Pandoc.hs
@@ -109,14 +109,14 @@ renderPandocWith ropt wopt item =
 --------------------------------------------------------------------------------
 -- | An extension of `renderPandocWith`, which allows you to specify a custom
 -- Pandoc transformation on the input `Item`.
--- Useful if you want to do your own transformations before running 
+-- Useful if you want to do your own transformations before running
 -- custom Pandoc transformations, e.g. using a `funcField` to transform raw content.
 renderPandocWithTransform :: ReaderOptions -> WriterOptions
                     -> (Pandoc -> Pandoc)
                     -> Item String
                     -> Compiler (Item String)
-renderPandocWithTransform ropt wopt f = 
-    renderPandocWithTransformM ropt wopt (return . f) 
+renderPandocWithTransform ropt wopt f =
+    renderPandocWithTransformM ropt wopt (return . f)
 
 
 --------------------------------------------------------------------------------
@@ -128,8 +128,8 @@ renderPandocWithTransformM :: ReaderOptions -> WriterOptions
                     -> (Pandoc -> Compiler Pandoc)
                     -> Item String
                     -> Compiler (Item String)
-renderPandocWithTransformM ropt wopt f i = 
-    writePandocWith wopt <$> (traverse f =<< readPandocWith ropt i) 
+renderPandocWithTransformM ropt wopt f i =
+    writePandocWith wopt <$> (traverse f =<< readPandocWith ropt i)
 
 
 --------------------------------------------------------------------------------
@@ -166,7 +166,7 @@ pandocCompilerWithTransform ropt wopt f =
 pandocCompilerWithTransformM :: ReaderOptions -> WriterOptions
                     -> (Pandoc -> Compiler Pandoc)
                     -> Compiler (Item String)
-pandocCompilerWithTransformM ropt wopt f = 
+pandocCompilerWithTransformM ropt wopt f =
     getResourceBody >>= renderPandocWithTransformM ropt wopt f
 
 
