1f9ba8bb3a4055da82e520349897f4d9ef4227b2
Jasper Van der Jeugt <m@jaspervdj.be>
2022-08-04 14:16:23 +0200

    Stub: async scheduler

--- lib/Hakyll/Core/Runtime.hs
+++ lib/Hakyll/Core/Runtime.hs
@@ -7,32 +7,25 @@ module Hakyll.Core.Runtime
 
 
 --------------------------------------------------------------------------------
-import           Control.Concurrent              (forkIO, getNumCapabilities,
-                                                  rtsSupportsBoundThreads)
-import           Control.Concurrent.Async.Lifted (forConcurrently)
-import           Control.Concurrent.MVar         (MVar, modifyMVar_, newMVar,
-                                                  readMVar)
-import qualified Control.Concurrent.MVar         as MVar
-import           Control.Monad                   (join, replicateM_, unless,
-                                                  void, when)
-import           Control.Monad.Except            (ExceptT, runExceptT,
-                                                  throwError)
-import           Control.Monad.Reader            (ReaderT, ask, runReaderT)
-import           Control.Monad.Trans             (liftIO)
-import           Data.Foldable                   (for_, traverse_)
-import           Data.IORef                      (IORef)
-import qualified Data.IORef                      as IORef
-import           Data.List                       (foldl', intercalate)
-import           Data.Map                        (Map)
-import qualified Data.Map                        as Map
-import           Data.Maybe                      (fromMaybe)
-import           Data.Sequence                   (Seq)
-import qualified Data.Sequence                   as Seq
-import           Data.Set                        (Set)
-import qualified Data.Set                        as Set
-import           Data.Traversable                (for)
-import           System.Exit                     (ExitCode (..))
-import           System.FilePath                 ((</>))
+import           Control.Concurrent            (forkIO, getNumCapabilities,
+                                                rtsSupportsBoundThreads)
+import qualified Control.Concurrent.MVar       as MVar
+import           Control.Monad                 (replicateM_, unless, void)
+import           Control.Monad.Reader          (ReaderT, ask, runReaderT)
+import           Control.Monad.Trans           (liftIO)
+import           Data.Foldable                 (for_, traverse_)
+import           Data.IORef                    (IORef)
+import qualified Data.IORef                    as IORef
+import           Data.List                     (foldl')
+import           Data.Map                      (Map)
+import qualified Data.Map                      as Map
+import           Data.Maybe                    (fromMaybe)
+import           Data.Sequence                 (Seq)
+import qualified Data.Sequence                 as Seq
+import           Data.Set                      (Set)
+import qualified Data.Set                      as Set
+import           System.Exit                   (ExitCode (..))
+import           System.FilePath               ((</>))
 
 
 --------------------------------------------------------------------------------
@@ -43,13 +36,13 @@ import           Hakyll.Core.Dependencies
 import           Hakyll.Core.Identifier
 import           Hakyll.Core.Item
 import           Hakyll.Core.Item.SomeItem
-import           Hakyll.Core.Logger              (Logger)
-import qualified Hakyll.Core.Logger              as Logger
+import           Hakyll.Core.Logger            (Logger)
+import qualified Hakyll.Core.Logger            as Logger
 import           Hakyll.Core.Provider
 import           Hakyll.Core.Routes
 import           Hakyll.Core.Rules.Internal
-import           Hakyll.Core.Store               (Store)
-import qualified Hakyll.Core.Store               as Store
+import           Hakyll.Core.Store             (Store)
+import qualified Hakyll.Core.Store             as Store
 import           Hakyll.Core.Util.File
 import           Hakyll.Core.Writable
 
@@ -82,14 +75,6 @@ run mode config logger rules = do
     let (oldFacts) = case mOldFacts of Store.Found f -> f
                                        _             -> mempty
 
-    state <- newMVar $ RuntimeState
-            { runtimeDone         = Set.empty
-            , runtimeSnapshots    = Set.empty
-            , runtimeTodo         = Map.empty
-            , runtimeFacts        = oldFacts
-            , runtimeDependencies = Map.empty
-            }
-
     -- Build runtime read/state
     scheduler <- IORef.newIORef $ emptyScheduler {schedulerFacts = oldFacts}
     let compilers = rulesCompilers ruleSet
@@ -97,7 +82,6 @@ run mode config logger rules = do
             { runtimeConfiguration = config
             , runtimeLogger        = logger
             , runtimeProvider      = provider
-            , runtimeState         = state
             , runtimeStore         = store
             , runtimeRoutes        = rulesRoutes ruleSet
             , runtimeUniverse      = Map.fromList compilers
@@ -105,7 +89,7 @@ run mode config logger rules = do
             }
 
     -- Run the program and fetch the resulting state
-    result <- runReaderT (build2 mode) read'
+    runReaderT (build mode) read'
     errors <- schedulerErrors <$> IORef.readIORef scheduler
     if null errors then do
         Logger.debug logger "Removing tmp directory..."
@@ -126,21 +110,10 @@ data RuntimeRead = RuntimeRead
     { runtimeConfiguration :: Configuration
     , runtimeLogger        :: Logger
     , runtimeProvider      :: Provider
-    , runtimeState         :: MVar RuntimeState
     , runtimeStore         :: Store
     , runtimeRoutes        :: Routes
     , runtimeUniverse      :: Map Identifier (Compiler SomeItem)
     , runtimeScheduler     :: IORef Scheduler
-    }
-
-
---------------------------------------------------------------------------------
-data RuntimeState = RuntimeState
-    { runtimeDone         :: Set Identifier
-    , runtimeSnapshots    :: Set (Identifier, Snapshot)
-    , runtimeTodo         :: Map Identifier (Compiler SomeItem)
-    , runtimeFacts        :: DependencyFacts
-    , runtimeDependencies :: Map Identifier (Set (Identifier, Snapshot))
     }
 
 
@@ -255,12 +228,6 @@ schedulerPop scheduler@Scheduler {..} = case Seq.viewl schedulerQueue of
 
 
 --------------------------------------------------------------------------------
-data Block
-    = BlockContinue
-    | BlockBlocked
-
-
---------------------------------------------------------------------------------
 schedulerBlock
     :: Identifier
     -> [(Identifier, Snapshot)]
@@ -349,44 +316,8 @@ schedulerWrite identifier depFacts scheduler0@Scheduler {..} =
 
 
 --------------------------------------------------------------------------------
-type Runtime a = ReaderT RuntimeRead (ExceptT String IO) a
-
-
---------------------------------------------------------------------------------
--- Because compilation of rules often revolves around IO,
--- be very careful when modifying the state
-modifyRuntimeState :: (RuntimeState -> RuntimeState) -> Runtime ()
-modifyRuntimeState f = liftIO . flip modifyMVar_ (pure . f) . runtimeState =<< ask
-
-
---------------------------------------------------------------------------------
-getRuntimeState :: Runtime RuntimeState
-getRuntimeState = liftIO . readMVar . runtimeState =<< ask
-
-
---------------------------------------------------------------------------------
-build :: RunMode -> Runtime ()
+build :: RunMode -> ReaderT RuntimeRead IO ()
 build mode = do
-    logger <- runtimeLogger <$> ask
-    Logger.header logger "Checking for out-of-date items"
-    scheduleOutOfDate
-    case mode of
-        RunModeNormal -> do
-            Logger.header logger "Compiling"
-            pickAndChase
-            Logger.header logger "Success"
-            facts <- runtimeFacts <$> getRuntimeState
-            store <- runtimeStore <$> ask
-            liftIO $ Store.set store factsKey facts
-        RunModePrintOutOfDate -> do
-            Logger.header logger "Out of date items:"
-            todo <- runtimeTodo <$> getRuntimeState
-            traverse_ (Logger.message logger . show) (Map.keys todo)
-
-
---------------------------------------------------------------------------------
-build2 :: RunMode -> ReaderT RuntimeRead IO ()
-build2 mode = do
     logger <- runtimeLogger <$> ask
     Logger.header logger "Checking for out-of-date items"
     schedulerRef <- runtimeScheduler <$> ask
@@ -394,7 +325,7 @@ build2 mode = do
     case mode of
         RunModeNormal -> do
             Logger.header logger "Compiling"
-            if rtsSupportsBoundThreads then pickAndChaseAsync else pickAndChase2
+            if rtsSupportsBoundThreads then pickAndChaseAsync else pickAndChase
             Logger.header logger "Success"
             facts <- liftIO $ schedulerFacts <$> IORef.readIORef schedulerRef
             store <- runtimeStore <$> ask
@@ -403,36 +334,6 @@ build2 mode = do
             Logger.header logger "Out of date items:"
             todo <- liftIO $ schedulerTodo <$> IORef.readIORef schedulerRef
             traverse_ (Logger.message logger . show) (Map.keys todo)
-
-
---------------------------------------------------------------------------------
-scheduleOutOfDate :: Runtime ()
-scheduleOutOfDate = do
-    logger   <- runtimeLogger   <$> ask
-    provider <- runtimeProvider <$> ask
-    universe <- runtimeUniverse <$> ask
-
-    let identifiers = Map.keys universe
-        modified    = Set.filter (resourceModified provider) (Map.keysSet universe)
-
-    state <- getRuntimeState
-    let facts = runtimeFacts state
-        todo  = runtimeTodo state
-        done  = runtimeDone state
-
-    let (ood, facts', msgs) = outOfDate identifiers modified facts
-        todo'               = Map.filterWithKey (\id' _ -> id' `Set.member` ood) universe
-        done'               = done `Set.union` (Map.keysSet universe `Set.difference` ood)
-
-    -- Print messages
-    mapM_ (Logger.debug logger) msgs
-
-    -- Update facts and todo items
-    modifyRuntimeState $ \s -> s
-        { runtimeDone  = done'
-        , runtimeTodo  = todo `Map.union` todo'
-        , runtimeFacts = facts'
-        }
 
 
 --------------------------------------------------------------------------------
@@ -451,23 +352,8 @@ scheduleOutOfDate2 = do
 
 
 --------------------------------------------------------------------------------
-pickAndChase :: Runtime ()
+pickAndChase :: ReaderT RuntimeRead IO ()
 pickAndChase = do
-    todo <- runtimeTodo <$> getRuntimeState
-    unless (null todo) $ do
-        acted <- mconcat <$> forConcurrently (Map.keys todo) chase
-        when (acted == Idled) $ do
-            -- This clause happens when chasing *every item* in `todo` resulted in
-            -- idling because tasks are all waiting on something: a dependency cycle
-            deps <- runtimeDependencies <$> getRuntimeState
-            throwError $ "Hakyll.Core.Runtime.pickAndChase: Dependency cycle detected: " ++
-                intercalate ", " [show k ++ " depends on " ++ show (Set.toList v) | (k, v) <- Map.toList deps]
-        pickAndChase
-
-
---------------------------------------------------------------------------------
-pickAndChase2 :: ReaderT RuntimeRead IO ()
-pickAndChase2 = do
     scheduler <- runtimeScheduler <$> ask
     pop       <- liftIO . IORef.atomicModifyIORef' scheduler $ schedulerPop
     go pop
@@ -486,7 +372,6 @@ pickAndChaseAsync = do
     liftIO $ do
         signal     <- MVar.newEmptyMVar
         numThreads <- getNumCapabilities
-        putStrLn $ "Running in " <> show numThreads <> " threads..."
 
         let spawnN :: Int -> IO ()
             spawnN n = replicateM_ n $ forkIO $ do
@@ -570,7 +455,6 @@ work id' compiler = do
                     liftIO $ write path item
                     Logger.debug logger $ "Routed to " ++ path
 
-            Logger.message logger $ "Saved _final for " <> show id'
             liftIO $ save store item
             liftIO . IORef.atomicModifyIORef' scheduler $
                 schedulerWrite id' facts
@@ -578,137 +462,3 @@ work id' compiler = do
         CompilerRequire reqs c -> do
             liftIO . IORef.atomicModifyIORef' scheduler $
                 schedulerBlock id' reqs c
-
-
---------------------------------------------------------------------------------
--- | Tracks whether a set of tasks has progressed overall (at least one task progressed)
--- or has idled
-data Progress = Progressed | Idled deriving (Eq)
-
-instance Semigroup Progress where
-    Idled      <> Idled      = Idled
-    Progressed <> _          = Progressed
-    _          <> Progressed = Progressed
-
-instance Monoid Progress where
-    mempty = Idled
-
-
---------------------------------------------------------------------------------
-chase :: Identifier -> Runtime Progress
-chase id' = do
-    logger    <- runtimeLogger        <$> ask
-    provider  <- runtimeProvider      <$> ask
-    universe  <- runtimeUniverse      <$> ask
-    routes    <- runtimeRoutes        <$> ask
-    store     <- runtimeStore         <$> ask
-    config    <- runtimeConfiguration <$> ask
-
-    state     <- getRuntimeState
-
-    Logger.debug logger $ "Processing " ++ show id'
-
-    let compiler = (runtimeTodo state) Map.! id'
-        read' = CompilerRead
-            { compilerConfig     = config
-            , compilerUnderlying = id'
-            , compilerProvider   = provider
-            , compilerUniverse   = Map.keysSet universe
-            , compilerRoutes     = routes
-            , compilerStore      = store
-            , compilerLogger     = logger
-            }
-
-    result <- liftIO $ runCompiler compiler read'
-    case result of
-        -- Rethrow error
-        CompilerError e -> throwError $ case compilerErrorMessages e of
-            [] -> "Compiler failed but no info given, try running with -v?"
-            es -> intercalate "; " es
-
-        -- Signal that a snapshot was saved ->
-        CompilerSnapshot snapshot c -> do
-            -- Update info. The next 'chase' will pick us again at some
-            -- point so we can continue then.
-            modifyRuntimeState $ \s -> s
-                { runtimeSnapshots = Set.insert (id', snapshot) (runtimeSnapshots s)
-                , runtimeTodo      = Map.insert id' c (runtimeTodo s)
-                }
-
-            return Progressed
-
-
-        -- Huge success
-        CompilerDone (SomeItem item) cwrite -> do
-            -- Print some info
-            let facts = compilerDependencies cwrite
-                cacheHits
-                    | compilerCacheHits cwrite <= 0 = "updated"
-                    | otherwise                     = "cached "
-            Logger.message logger $ cacheHits ++ " " ++ show id'
-
-            -- Sanity check
-            unless (itemIdentifier item == id') $ throwError $
-                "The compiler yielded an Item with Identifier " ++
-                show (itemIdentifier item) ++ ", but we were expecting " ++
-                "an Item with Identifier " ++ show id' ++ " " ++
-                "(you probably want to call makeItem to solve this problem)"
-
-            -- Write if necessary
-            (mroute, _) <- liftIO $ runRoutes routes provider id'
-            case mroute of
-                Nothing    -> return ()
-                Just route -> do
-                    let path = destinationDirectory config </> route
-                    liftIO $ makeDirectories path
-                    liftIO $ write path item
-                    Logger.debug logger $ "Routed to " ++ path
-
-            -- Save! (For load)
-            liftIO $ save store item
-
-            modifyRuntimeState $ \s -> s
-                { runtimeDone         = Set.insert id' (runtimeDone s)
-                , runtimeTodo         = Map.delete id' (runtimeTodo s)
-                , runtimeFacts        = Map.insert id' facts (runtimeFacts s)
-                , runtimeDependencies = Map.delete id' (runtimeDependencies s)
-                }
-
-            return Progressed
-
-        -- Try something else first
-        CompilerRequire reqs c -> do
-            let done      = runtimeDone state
-                snapshots = runtimeSnapshots state
-
-            deps <- fmap join . for reqs $ \(depId, depSnapshot) -> do
-                Logger.debug logger $
-                    "Compiler requirement found for: " ++ show id' ++
-                    ": " ++ show depId ++ " (snapshot " ++ depSnapshot ++ ")"
-
-                -- Done if we either completed the entire item (runtimeDone) or
-                -- if we previously saved the snapshot (runtimeSnapshots).
-                let depDone =
-                        depId `Set.member` done ||
-                        (depId, depSnapshot) `Set.member` snapshots
-                    actualDep = [(depId, depSnapshot) | not depDone]
-
-                return actualDep
-
-            modifyRuntimeState $ \s -> s
-                { runtimeTodo         = Map.insert id'
-                    (if null deps then c else compilerResult result)
-                    (runtimeTodo s)
-                 -- We track dependencies only to inform users when an infinite loop is detected
-                , runtimeDependencies = Map.insertWith Set.union id' (Set.fromList deps) (runtimeDependencies s)
-                }
-
-            -- Progress has been made if at least one of the
-            -- requirements can move forwards at the next pass
-            -- In some cases, dependencies have been processed in parallel in which case `deps`
-            -- can be empty, and we can progress to the next stage. See issue #907
-            let progress | null deps    = Progressed
-                         | deps == reqs = Idled
-                         | otherwise    = Progressed
-
-            return progress
