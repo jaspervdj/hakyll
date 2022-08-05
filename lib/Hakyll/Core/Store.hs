f5316936f2594f275347e9dadcd3653c1e80e361
Jasper Van der Jeugt <m@jaspervdj.be>
2022-08-05 14:38:44 +0200

    fix: IORef to skip concurrent writes in Store.set
    See #903

--- lib/Hakyll/Core/Store.hs
+++ lib/Hakyll/Core/Store.hs
@@ -16,14 +16,18 @@ module Hakyll.Core.Store
 
 
 --------------------------------------------------------------------------------
-import qualified Data.Hashable        as DH
+import           Control.Monad        (when)
 import           Data.Binary          (Binary, decode, encodeFile)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Cache.LRU.IO    as Lru
+import qualified Data.Hashable        as DH
+import qualified Data.IORef           as IORef
 import           Data.List            (intercalate)
 import           Data.Maybe           (isJust)
+import qualified Data.Set             as Set
 import           Data.Typeable        (TypeRep, Typeable, cast, typeOf)
-import           System.Directory     (createDirectoryIfMissing, doesFileExist, removeFile)
+import           System.Directory     (createDirectoryIfMissing, doesFileExist,
+                                       removeFile)
 import           System.FilePath      ((</>))
 import           System.IO            (IOMode (..), hClose, openFile)
 import           System.IO.Error      (catchIOError, ioeSetFileName,
@@ -38,9 +42,11 @@ data Box = forall a. Typeable a => Box a
 --------------------------------------------------------------------------------
 data Store = Store
     { -- | All items are stored on the filesystem
-      storeDirectory :: FilePath
-    , -- | Optionally, items are also kept in-memory
-      storeMap       :: Maybe (Lru.AtomicLRU FilePath Box)
+      storeDirectory        :: FilePath
+    , -- | See 'set'
+      storeDirectoryWriting :: IORef.IORef (Set.Set String)
+      -- | Optionally, items are also kept in-memory
+    , storeMap              :: Maybe (Lru.AtomicLRU FilePath Box)
     }
 
 
@@ -72,10 +78,12 @@ new :: Bool      -- ^ Use in-memory caching
     -> IO Store  -- ^ Store
 new inMemory directory = do
     createDirectoryIfMissing True directory
+    writeLock <- IORef.newIORef Set.empty
     ref <- if inMemory then Just <$> Lru.newAtomicLRU csize else return Nothing
     return Store
-        { storeDirectory = directory
-        , storeMap       = ref
+        { storeDirectory        = directory
+        , storeDirectoryWriting = writeLock
+        , storeMap              = ref
         }
   where
     csize = Just 500
@@ -92,16 +100,16 @@ withStore store loc run identifier = modifyIOError handle $ run key path
 --------------------------------------------------------------------------------
 -- | Auxiliary: add an item to the in-memory cache
 cacheInsert :: Typeable a => Store -> String -> a -> IO ()
-cacheInsert (Store _ Nothing)    _   _     = return ()
-cacheInsert (Store _ (Just lru)) key x =
+cacheInsert (Store _ _ Nothing)    _   _     = return ()
+cacheInsert (Store _ _ (Just lru)) key x =
     Lru.insert key (Box x) lru
 
 
 --------------------------------------------------------------------------------
 -- | Auxiliary: get an item from the in-memory cache
 cacheLookup :: forall a. Typeable a => Store -> String -> IO (Result a)
-cacheLookup (Store _ Nothing)    _   = return NotFound
-cacheLookup (Store _ (Just lru)) key = do
+cacheLookup (Store _ _ Nothing)    _   = return NotFound
+cacheLookup (Store _ _ (Just lru)) key = do
     res <- Lru.lookup key lru
     return $ case res of
         Nothing      -> NotFound
@@ -112,15 +120,15 @@ cacheLookup (Store _ (Just lru)) key = do
 
 --------------------------------------------------------------------------------
 cacheIsMember :: Store -> String -> IO Bool
-cacheIsMember (Store _ Nothing)    _   = return False
-cacheIsMember (Store _ (Just lru)) key = isJust <$> Lru.lookup key lru
+cacheIsMember (Store _ _ Nothing)    _   = return False
+cacheIsMember (Store _ _ (Just lru)) key = isJust <$> Lru.lookup key lru
 
 
 --------------------------------------------------------------------------------
 -- | Auxiliary: delete an item from the in-memory cache
 cacheDelete :: Store -> String -> IO ()
-cacheDelete (Store _ Nothing)    _   = return ()
-cacheDelete (Store _ (Just lru)) key = do
+cacheDelete (Store _ _ Nothing)    _   = return ()
+cacheDelete (Store _ _ (Just lru)) key = do
     _ <- Lru.delete key lru
     return ()
 
@@ -129,7 +137,22 @@ cacheDelete (Store _ (Just lru)) key = do
 -- | Store an item
 set :: (Binary a, Typeable a) => Store -> [String] -> a -> IO ()
 set store identifier value = withStore store "set" (\key path -> do
-    encodeFile path value
+    -- We need to avoid concurrent writes to the filesystem.  Imagine the
+    -- follow scenario:
+    --
+    --  *  We compile multiple posts
+    --  *  All of these fetch some common metadata
+    --  *  This metadata is missing; we fetch it and then store it.
+    --
+    -- To solve this, we skip duplicate writes by tracking their status
+    -- in 'storeDirectoryWriting'.  Since this set will usually be small, the
+    -- required locking should be fast.  Additionally the actual IO operation
+    -- still happens outside of the locking.
+    first <- IORef.atomicModifyIORef' (storeDirectoryWriting store) $ \writing ->
+        (Set.insert key writing, Set.notMember key writing)
+    when first $ encodeFile path value
+    IORef.atomicModifyIORef' (storeDirectoryWriting store) $ \writing ->
+        (if first then Set.delete key writing else writing, ())
     cacheInsert store key value
   ) identifier
 
