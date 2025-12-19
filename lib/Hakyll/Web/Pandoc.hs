db3feb16d49b48b5ddd56e1fd54fc27d5f18583e
sven mie <45923502+sss-create@users.noreply.github.com>
2025-12-19 18:03:43 +0100

    added asciidoc support (#1089)
    * Added asciidoc support for pandoc 3.8.3+

--- lib/Hakyll/Web/Pandoc.hs
+++ lib/Hakyll/Web/Pandoc.hs
@@ -68,6 +68,9 @@ readPandocWith ropt item =
         MediaWiki          -> readMediaWiki ro
         OrgMode            -> readOrg ro
         Rst                -> readRST ro
+#if MIN_VERSION_pandoc(3,8,3)
+        AsciiDoc           -> readAsciiDoc ro
+#endif
 -- This preprocessing instruction can be dropped
 -- once the minimum supported GHC version is 8.10
 #if MIN_VERSION_pandoc(3,1,3)
