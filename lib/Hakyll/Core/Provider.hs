jaspervdj/hakyll

commit e4b2dfd7d7fe8a6be163638efc8f7f0c9558afbb

Add support for declaring metadata-only dependencies (#1084)
* Provider: Classify identifiers for which only the metadata changed

This is the foundation work required to introduce dependencies only on
metadata. Often times, a post's body/content may change, but its
metadata remains unmodified. Some compilers only need to be re-run if
the metadata changes (e.g., consider a sidebar that includes all post
tags). Presently, Hakyll does not allow depending on metadata, thereby
incurring a lot of unnecessary in such a scenario.

This commit allows classification of identifiers for which only the
metadata changed. The next commit will add an API to only depend on the
metadata of an identifier.

In technical terms it is implemented as follows: When invalidating the
metadataCache, retrieve the old metadata values. Afterward, for
identifiers modified (according to resourceModified), load the new
metadata and compare it to the old. Only consider metadata to have
changed if it differs.

* Refactor the Dependency type to allow metadata-only dependencies

This is an API-breaking change, it refactors the old Dependency type
into three types: A Dependency, a DependencyKind, and a
DependencySelector. Through the DependencyKind we can specify a
dependency on either the Metadata or the Content of another Identifier.

The DependencySelector allows us, for both DependencyKinds, to either
depend on a single Identifier or a Pattern (as before). Dependencies of
KindMetadata are only considered out-of-date if the metadata changed.

This is extremely useful in many scenarios where Hakyll previously
induced unnecessary rebuilds because it could not distinguish between a
content and a metadata dependency. As an example, consider a navigation
sidebar displaying all available tags. There are also examples in the
existing code base where I have now used a metadata-only dependency
(getRoute, compilerGetMetadata, buildTagsWith).

lib/Hakyll/Core/Provider.hs
@@ -22,22 +22,35 @@ module Hakyll.Core.Provider
     , Internal.resourceBody
     ) where
 
-
 --------------------------------------------------------------------------------
 import qualified Hakyll.Core.Provider.Internal      as Internal
 import qualified Hakyll.Core.Provider.MetadataCache as Internal
 import           Hakyll.Core.Store                  (Store)
+import           Hakyll.Core.Identifier             (Identifier)
 
 
 --------------------------------------------------------------------------------
 -- | Create a resource provider
-newProvider :: Store                   -- ^ Store to use
-            -> (FilePath -> IO Bool)   -- ^ Should we ignore this file?
-            -> FilePath                -- ^ Search directory
-            -> IO Internal.Provider    -- ^ Resulting provider
+newProvider :: Store                                -- ^ Store to use
+            -> (FilePath -> IO Bool)                -- ^ Should we ignore this file?
+            -> FilePath                             -- ^ Search directory
+            -> IO (Internal.Provider, [Identifier]) -- ^ Resulting provider and modified metadata
 newProvider store ignore directory = do
-    -- Delete metadata cache where necessary
-    p <- Internal.newProvider store ignore directory
-    mapM_ (Internal.resourceInvalidateMetadataCache p) $
-        filter (Internal.resourceModified p) $ Internal.resourceList p
-    return p
+  p <- Internal.newProvider store ignore directory
+
+  let modified =
+        filter
+          (Internal.resourceModified p)
+          (Internal.resourceList p)
+
+  -- Delete metadata cache where necessary and extract identifiers for which the
+  -- metadata was actually modified. That is, exclude identifiers where the body
+  -- was modified but the metadata wasn't.
+  mayModifiedMetadata <- mapM (Internal.resourceInvalidateMetadataCache p) modified
+  newMetadata <- mapM (Internal.resourceMetadata p) modified
+  let modifiedMetadata =
+        map (\(i, _, _) -> i) $
+          filter (\(_, nm, om) -> nm /= om) $
+            zip3 modified mayModifiedMetadata newMetadata
+
+  return (p, modifiedMetadata)

lib/Hakyll/Core/Provider/MetadataCache.hs
@@ -37,10 +37,19 @@ resourceBody p r = do
 
 
 --------------------------------------------------------------------------------
-resourceInvalidateMetadataCache :: Provider -> Identifier -> IO ()
+resourceInvalidateMetadataCache :: Provider -> Identifier -> IO (Metadata)
 resourceInvalidateMetadataCache p r = do
+    result <- Store.get (providerStore p)
+        [name, toFilePath r, "metadata"]
+    let md = case result of
+          Store.Found (BinaryMetadata m) -> m
+          Store.NotFound -> mempty
+          Store.WrongType _ _ -> error "unexpected WrongType"
+
     Store.delete (providerStore p) [name, toFilePath r, "metadata"]
     Store.delete (providerStore p) [name, toFilePath r, "body"]
+
+    pure md
 
 
 --------------------------------------------------------------------------------

lib/Hakyll/Core/Compiler.hs
@@ -82,7 +82,7 @@ getRoute identifier = do
     -- Note that this makes us dependend on that identifier: when the metadata
     -- of that item changes, the route may change, hence we have to recompile
     (mfp, um) <- compilerUnsafeIO $ runRoutes routes provider identifier
-    when um $ compilerTellDependencies [IdentifierDependency identifier]
+    when um $ compilerTellDependencies [metadataDependency $ IdentifierDependency identifier]
     return mfp
 
 

lib/Hakyll/Core/Compiler/Internal.hs
@@ -346,7 +346,7 @@ compilerTellCacheHits ch = compilerTell mempty {compilerCacheHits = ch}
 compilerGetMetadata :: Identifier -> Compiler Metadata
 compilerGetMetadata identifier = do
     provider <- compilerProvider <$> compilerAsk
-    compilerTellDependencies [IdentifierDependency identifier]
+    compilerTellDependencies [metadataDependency $ IdentifierDependency identifier]
     compilerUnsafeIO $ resourceMetadata provider identifier
 
 
@@ -355,5 +355,5 @@ compilerGetMatches :: Pattern -> Compiler [Identifier]
 compilerGetMatches pattern = do
     universe <- compilerUniverse <$> compilerAsk
     let matching = S.filter (matches pattern) universe
-    compilerTellDependencies [PatternDependency pattern matching]
+    compilerTellDependencies [contentDependency $ PatternDependency pattern matching]
     pure $ S.toList matching

lib/Hakyll/Core/Compiler/Require.hs
@@ -105,7 +105,7 @@ loadSnapshotCollection ids = do
             when (id' `S.notMember` universe) (fail $ notFound id' snap)
     traverse_ checkMember ids
 
-    compilerTellDependencies $ IdentifierDependency . fst <$> toList ids
+    compilerTellDependencies $ contentDependency . IdentifierDependency . fst <$> toList ids
     let go (id', snap) = do
             result <- compilerUnsafeIO $ Store.get store (key id' snap)
             case result of

lib/Hakyll/Core/Dependencies.hs
@@ -1,9 +1,14 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TupleSections #-}
 module Hakyll.Core.Dependencies
     ( Dependency (..)
+    , DependencySelector (..)
+    , DependencyKind (..)
     , DependencyFacts
     , outOfDate
+    , contentDependency
+    , metadataDependency
     ) where
 
 
@@ -15,6 +20,7 @@ import qualified Control.Monad.State            as State
 import           Control.Monad.Writer           (tell)
 import           Data.Binary                    (Binary (..), getWord8,
                                                  putWord8)
+import           Data.Functor                   ((<&>))
 import           Data.List                      (find)
 import           Data.Map                       (Map)
 import qualified Data.Map                       as M
@@ -30,23 +36,54 @@ import           Hakyll.Core.Identifier.Pattern
 
 
 --------------------------------------------------------------------------------
-data Dependency
+data DependencySelector
     = PatternDependency Pattern (Set Identifier)
     | IdentifierDependency Identifier
+    deriving (Show, Typeable)
+
+--------------------------------------------------------------------------------
+instance Binary DependencySelector where
+    put (PatternDependency p is) = putWord8 0 >> put p >> put is
+    put (IdentifierDependency i) = putWord8 1 >> put i
+    get = getWord8 >>= \t -> case t of
+        0 -> PatternDependency <$> get <*> get
+        1 -> IdentifierDependency <$> get
+        _ -> fail "Data.Binary.get: Invalid DependencySelector"
+
+
+--------------------------------------------------------------------------------
+-- | A data type representing a dependency on another 'Identifier'. We can
+-- depend either on the 'Hakyll.Core.Metadata.Metadata' or the entire content of
+-- the underlying file. This is signified by the supplied 'DependencyKind'.
+data Dependency
+    = Dependency DependencyKind DependencySelector
     | AlwaysOutOfDate
     deriving (Show, Typeable)
 
 
 --------------------------------------------------------------------------------
+-- | Utility function to create a new content dependency.
+contentDependency :: DependencySelector -> Dependency
+contentDependency = Dependency KindContent
+
+-- | Utility function to a create a new metadata dependency.
+metadataDependency :: DependencySelector -> Dependency
+metadataDependency = Dependency KindMetadata
+
+
+--------------------------------------------------------------------------------
 instance Binary Dependency where
-    put (PatternDependency p is) = putWord8 0 >> put p >> put is
-    put (IdentifierDependency i) = putWord8 1 >> put i
     put AlwaysOutOfDate = putWord8 2
+    put (Dependency k s) = putWord8 3 >> put k >> put s
+
     get = getWord8 >>= \t -> case t of
-        0 -> PatternDependency <$> get <*> get
-        1 -> IdentifierDependency <$> get
+        -- XXX: Backwards compatability with Hakyll <=4.16.7.1.
+        0 -> (\p i -> contentDependency $ PatternDependency p i) <$> get <*> get
+        1 -> contentDependency . IdentifierDependency <$> get
+
         2 -> pure AlwaysOutOfDate
-        _ -> error "Data.Binary.get: Invalid Dependency"
+        3 -> Dependency <$> get <*> get
+        _ -> fail "Data.Binary.get: Invalid Dependency"
 
 
 --------------------------------------------------------------------------------
@@ -56,11 +93,12 @@ type DependencyFacts = Map Identifier [Dependency]
 --------------------------------------------------------------------------------
 outOfDate
     :: [Identifier]     -- ^ All known identifiers
-    -> Set Identifier   -- ^ Initially out-of-date resources
+    -> Set Identifier   -- ^ Changed content
+    -> Set Identifier   -- ^ Changed metadata
     -> DependencyFacts  -- ^ Old dependency facts
     -> (Set Identifier, DependencyFacts, [String])
-outOfDate universe ood oldFacts =
-    let (_, state, logs) = runRWS rws universe (DependencyState oldFacts ood)
+outOfDate universe ood oodMeta oldFacts =
+    let (_, state, logs) = runRWS rws universe (DependencyState oldFacts ood oodMeta)
     in (dependencyOod state, dependencyFacts state, logs)
   where
     rws = do
@@ -71,8 +109,9 @@ outOfDate universe ood oldFacts =
 
 --------------------------------------------------------------------------------
 data DependencyState = DependencyState
-    { dependencyFacts :: DependencyFacts
-    , dependencyOod   :: Set Identifier
+    { dependencyFacts   :: DependencyFacts
+    , dependencyOod     :: Set Identifier
+    , dependencyOodMeta :: Set Identifier
     } deriving (Show)
 
 
@@ -87,10 +126,23 @@ markOod id' = State.modify $ \s ->
 
 
 --------------------------------------------------------------------------------
+data DependencyKind = KindContent | KindMetadata
+  deriving (Show)
+
+instance Binary DependencyKind where
+  put KindContent = putWord8 0
+  put KindMetadata = putWord8 1
+
+  get = getWord8 >>= \t -> case t of
+      0 -> pure KindContent
+      1 -> pure KindMetadata
+      _ -> fail "Data.Binary.get: Invalid DependencyKind"
+
+--------------------------------------------------------------------------------
 -- | Collection of dependencies that should be checked to determine
 -- if an identifier needs rebuilding.
 data Dependencies
-  = DependsOn [Identifier]
+  = DependsOn [(DependencyKind, Identifier)]
   | MustRebuild
   deriving (Show)
 
@@ -108,9 +160,12 @@ dependenciesFor id' = do
     facts <- dependencyFacts <$> State.get
     return $ foldMap dependenciesFor' $ fromMaybe [] $ M.lookup id' facts
   where
-    dependenciesFor' (IdentifierDependency i) = DependsOn [i]
-    dependenciesFor' (PatternDependency _ is) = DependsOn $ S.toList is
-    dependenciesFor' AlwaysOutOfDate          = MustRebuild
+    dependenciesForSelector (IdentifierDependency i) = [i]
+    dependenciesForSelector (PatternDependency _ is) = S.toList is
+
+    dependenciesFor' AlwaysOutOfDate            = MustRebuild
+    dependenciesFor' (Dependency kind selector) = DependsOn $
+        map (kind,) $ dependenciesForSelector selector
 
 
 --------------------------------------------------------------------------------
@@ -132,17 +187,19 @@ checkChangedPatterns = do
         State.modify $ \s -> s
             {dependencyFacts = M.insert id' deps' $ dependencyFacts s}
   where
-    go _   ds (IdentifierDependency i) = return $ IdentifierDependency i : ds
-    go _   ds AlwaysOutOfDate          = return $ AlwaysOutOfDate : ds
-    go id' ds (PatternDependency p ls) = do
+    go' _   (IdentifierDependency i) = return $ IdentifierDependency i
+    go' id' (PatternDependency p ls) = do
         universe <- ask
         let ls' = S.fromList $ filterMatches p universe
         if ls == ls'
-            then return $ PatternDependency p ls : ds
+            then return $ PatternDependency p ls
             else do
                 tell [show id' ++ " is out-of-date because a pattern changed"]
                 markOod id'
-                return $ PatternDependency p ls' : ds
+                return $ PatternDependency p ls'
+
+    go _   ds AlwaysOutOfDate          = return $ AlwaysOutOfDate : ds
+    go id' ds (Dependency kind select) = (Dependency kind <$> go' id' select) <&> (: ds)
 
 
 --------------------------------------------------------------------------------
@@ -155,12 +212,19 @@ bruteForce = do
         (todo', changed) <- foldM check ([], False) todo
         when changed (go todo')
 
+    findOod oodContent oodMetadata (k, i)
+      = S.member i $
+          case k of
+            KindContent  -> oodContent
+            KindMetadata -> oodMetadata
+
     check (todo, changed) id' = do
         deps <- dependenciesFor id'
         case deps of
           DependsOn depList -> do
-            ood  <- dependencyOod <$> State.get
-            case find (`S.member` ood) depList of
+            ood     <- dependencyOod <$> State.get
+            oodMeta <- dependencyOodMeta <$> State.get
+            case find (findOod ood oodMeta) depList of
                 Nothing -> return (id' : todo, changed)
                 Just d  -> do
                     tell [show id' ++ " is out-of-date because " ++

lib/Hakyll/Core/Metadata.hs
@@ -84,10 +84,10 @@ getMetadataField' identifier key = do
 
 
 --------------------------------------------------------------------------------
-makePatternDependency :: MonadMetadata m => Pattern -> m Dependency
-makePatternDependency pattern = do
+makePatternDependency :: MonadMetadata m => DependencyKind -> Pattern -> m Dependency
+makePatternDependency kind pattern = do
     matches' <- getMatches pattern
-    return $ PatternDependency pattern (S.fromList matches')
+    return $ Dependency kind (PatternDependency pattern (S.fromList matches'))
 
 
 --------------------------------------------------------------------------------

lib/Hakyll/Core/Runtime.hs
@@ -71,7 +71,7 @@ run mode config logger rules = do
     Logger.message logger "Creating store..."
     store <- Store.new (inMemoryCache config) $ storeDirectory config
     Logger.message logger "Creating provider..."
-    provider <- newProvider store (shouldIgnoreFile config) $
+    (provider, modifiedMeta) <- newProvider store (shouldIgnoreFile config) $
         providerDirectory config
     Logger.message logger "Running rules..."
     ruleSet  <- runRules rules provider
@@ -88,6 +88,7 @@ run mode config logger rules = do
             { runtimeConfiguration = config
             , runtimeLogger        = logger
             , runtimeProvider      = provider
+            , runtimeModifiedMeta  = Set.fromList modifiedMeta
             , runtimeStore         = store
             , runtimeRoutes        = rulesRoutes ruleSet
             , runtimeUniverse      = Map.fromList compilers
@@ -116,6 +117,7 @@ data RuntimeRead = RuntimeRead
     { runtimeConfiguration :: Configuration
     , runtimeLogger        :: Logger
     , runtimeProvider      :: Provider
+    , runtimeModifiedMeta  :: Set Identifier
     , runtimeStore         :: Store
     , runtimeRoutes        :: Routes
     , runtimeUniverse      :: Map Identifier (Compiler SomeItem)
@@ -185,9 +187,10 @@ schedulerError i e s = (s {schedulerErrors = (i, e) : schedulerErrors s}, ())
 schedulerMarkOutOfDate
     :: Map Identifier (Compiler SomeItem)
     -> Set Identifier
+    -> Set Identifier
     -> Scheduler
     -> (Scheduler, [String])
-schedulerMarkOutOfDate universe modified scheduler@Scheduler {..} =
+schedulerMarkOutOfDate universe modifiedC modifiedM scheduler@Scheduler {..} =
     ( scheduler
         { schedulerQueue = schedulerQueue <> Seq.fromList (Map.keys todo)
         , schedulerDone  = schedulerDone <>
@@ -198,7 +201,7 @@ schedulerMarkOutOfDate universe modified scheduler@Scheduler {..} =
     , msgs
     )
   where
-    (ood, facts', msgs) = outOfDate (Map.keys universe) modified schedulerFacts
+    (ood, facts', msgs) = outOfDate (Map.keys universe) modifiedC modifiedM schedulerFacts
     todo = Map.filterWithKey (\id' _ -> id' `Set.member` ood) universe
 
 
@@ -396,13 +399,14 @@ build mode = do
 --------------------------------------------------------------------------------
 scheduleOutOfDate :: ReaderT RuntimeRead IO ()
 scheduleOutOfDate = do
-    logger       <- runtimeLogger    <$> ask
-    provider     <- runtimeProvider  <$> ask
-    universe     <- runtimeUniverse  <$> ask
-    schedulerRef <- runtimeScheduler <$> ask
-    let modified  = Set.filter (resourceModified provider) (Map.keysSet universe)
+    logger       <- runtimeLogger       <$> ask
+    provider     <- runtimeProvider     <$> ask
+    modifiedMeta <- runtimeModifiedMeta <$> ask
+    universe     <- runtimeUniverse     <$> ask
+    schedulerRef <- runtimeScheduler    <$> ask
+    let modifiedContent = Set.filter (resourceModified provider) (Map.keysSet universe)
     msgs <- liftIO . IORef.atomicModifyIORef' schedulerRef $
-        schedulerMarkOutOfDate universe modified
+        schedulerMarkOutOfDate universe modifiedContent modifiedMeta
 
     -- Print messages
     mapM_ (Logger.debug logger) msgs

commit 86621ff29982dc4a85cddde3c80fe54295482b2e

Metadata invaliation: check only files that previously had metadata (#1093)
While trying to regenerate the hakyll website, I found that it errors out with the following error: 

```
hakyll-website: ./site.hs: YAML parse exception at line 3, column 0,
while parsing a flow mapping:
did not find expected ',' or '}'
HasCallStack backtrace:
  collectBacktraces, called at libraries/ghc-internal/src/GHC/Internal/Exception.hs:169:13 in ghc-internal:GHC.Internal.Exception
  toExceptionWithBacktrace, called at libraries/ghc-internal/src/GHC/Internal/IO.hs:260:11 in ghc-internal:GHC.Internal.IO
  throwIO, called at lib/Hakyll/Core/Provider/Metadata.hs:57:22 in hakyll-4.16.7.1-inplace:Hakyll.Core.Provider.Metadata
```

Indeed, `web/site.hs` starts with `-----`, but what follows below is not valid YAML ([just Haskell code](https://github.com/jaspervdj/hakyll/blob/ec95ad77ebc2188aec39e9d3ecd60fe28da31a0f/web/site.hs#L1-L11)).  This did work at some point before, so I did some bisecting and found that it broke in e4b2dfd7d7fe8a6be163638efc8f7f0c9558afbb.

The concrete issue seems to be that the new metadata handling seems to load metadata from all files known to the provider, rather than just those that are actually used.

lib/Hakyll/Core/Provider.hs
@@ -23,6 +23,7 @@ module Hakyll.Core.Provider
     ) where
 
 --------------------------------------------------------------------------------
+import           Control.Monad                      (forM)
 import qualified Hakyll.Core.Provider.Internal      as Internal
 import qualified Hakyll.Core.Provider.MetadataCache as Internal
 import           Hakyll.Core.Store                  (Store)
@@ -46,11 +47,13 @@ newProvider store ignore directory = do
   -- Delete metadata cache where necessary and extract identifiers for which the
   -- metadata was actually modified. That is, exclude identifiers where the body
   -- was modified but the metadata wasn't.
-  mayModifiedMetadata <- mapM (Internal.resourceInvalidateMetadataCache p) modified
-  newMetadata <- mapM (Internal.resourceMetadata p) modified
-  let modifiedMetadata =
-        map (\(i, _, _) -> i) $
-          filter (\(_, nm, om) -> nm /= om) $
-            zip3 modified mayModifiedMetadata newMetadata
+  modifiedMetadata <- fmap concat $ forM modified $ \identifier -> do
+    mbCachedMetadata <- Internal.resourceLookupMetadataCache p identifier
+    case mbCachedMetadata of
+      Nothing -> pure []
+      Just oldMeta -> do
+        Internal.resourceInvalidateMetadataCache p identifier
+        newMeta <- Internal.resourceMetadata p identifier
+        pure $ if oldMeta == newMeta then [] else [identifier]
 
   return (p, modifiedMetadata)

lib/Hakyll/Core/Provider/MetadataCache.hs
@@ -2,12 +2,14 @@
 module Hakyll.Core.Provider.MetadataCache
     ( resourceMetadata
     , resourceBody
+    , resourceLookupMetadataCache
     , resourceInvalidateMetadataCache
     ) where
 
 
 --------------------------------------------------------------------------------
 import           Control.Monad                 (unless)
+import           Data.Maybe                    (fromMaybe)
 import           Hakyll.Core.Identifier
 import           Hakyll.Core.Metadata
 import           Hakyll.Core.Provider.Internal
@@ -22,9 +24,7 @@ resourceMetadata p r
     | otherwise                = do
         -- TODO keep time in md cache
         load p r
-        Store.Found (BinaryMetadata md) <- Store.get (providerStore p)
-            [name, toFilePath r, "metadata"]
-        return md
+        fromMaybe mempty <$> resourceLookupMetadataCache p r
 
 
 --------------------------------------------------------------------------------
@@ -37,19 +37,22 @@ resourceBody p r = do
 
 
 --------------------------------------------------------------------------------
-resourceInvalidateMetadataCache :: Provider -> Identifier -> IO (Metadata)
-resourceInvalidateMetadataCache p r = do
-    result <- Store.get (providerStore p)
-        [name, toFilePath r, "metadata"]
-    let md = case result of
-          Store.Found (BinaryMetadata m) -> m
-          Store.NotFound -> mempty
-          Store.WrongType _ _ -> error "unexpected WrongType"
+-- | Perform a lookup for metadata in the cache only.
+-- Useful internally for invalidation.
+resourceLookupMetadataCache :: Provider -> Identifier -> IO (Maybe Metadata)
+resourceLookupMetadataCache p r = do
+    result <- Store.get (providerStore p) [name, toFilePath r, "metadata"]
+    pure $ case result of
+      Store.Found (BinaryMetadata m) -> Just m
+      Store.NotFound                 -> Nothing
+      Store.WrongType _ _            -> error "unexpected WrongType"
 
+
+--------------------------------------------------------------------------------
+resourceInvalidateMetadataCache :: Provider -> Identifier -> IO ()
+resourceInvalidateMetadataCache p r = do
     Store.delete (providerStore p) [name, toFilePath r, "metadata"]
     Store.delete (providerStore p) [name, toFilePath r, "body"]
-
-    pure md
 
 
 --------------------------------------------------------------------------------